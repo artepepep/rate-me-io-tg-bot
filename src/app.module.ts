f5757bd7705796996e755e37db88991966d40a8a
mac <Artem1884@outlook.com>
2026-01-30 20:53:48 +0100

    feat: Configure nestjs-telegraf module. Make start command.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { session } from 'telegraf';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TelegrafModule.forRoot({
+      middlewares: [session()],
+      token: process.env.BOT_TOKEN!,
+    }),
+  ],
   providers: [AppService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Command, Update } from 'nestjs-telegraf';
+import { Context } from 'telegraf';
 
 @Injectable()
+@Update()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  @Command('start')
+  async startCommand(ctx: Context) {
+    await ctx.reply('Welcome to the bot');
   }
 }
